fd252efbcc5515c49fc301eaea958bc010e4f58b
unhang <unhang11@gmail.com>
2020-02-11 11:59:16 +0700

    init new web app

---
+++ App.js
@@ -0,0 +1,10 @@
+const Component = require(React.Component);
+require(React);
+
+class App extends Component {
+    render() {
+        return (<h1>Helloo</h1>);
+    }
+}
+
+// exports default App;
---
+++ main.js
@@ -0,0 +1,11 @@
+
+const ReactDOM = require(ReactDOM);
+
+// const App = require('./App.js');
+
+
+
+ReactDOM.render(
+    <h1>HELOO</h1>,
+    document.getElementById(root)
+);
